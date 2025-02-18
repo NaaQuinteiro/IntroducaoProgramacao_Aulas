d97b2efd509e8676aea876b7395049051ba70e91
NaaQuinteiro <thaynaquinteiro@gmail.com>
2025-02-18 19:56:24 -0300

    Aulas 1, Aula 2, Aula 3

--- Aula01/script.py
+++ Aula01/script.py
@@ -40,19 +40,4 @@
 # raiz = math.sqrt(numero1) #aqui eu não poderia passar o numero 2 pq ele é string e a função não aceita strings,ele n da erro aqui mas quando o codigo é executado ele quebra, requisitando um valor do tipo numero reais
 # print(raiz)
 
-#--------------- AULA 02 --------------
-
-number1 = float(input("Digite um numero: "))
-
-number2 = float(input("Digite um numero: "))
-
-number3 = float(input("Digite um numero: "))
-
-media = (number1 + number2 + number3) / 3
-
-
-print(f"A média dos numeros {number1}, {number2} e {number3}: {media}") #usa a formatação padrão
-
-print(f"A média dos numeros {number1}, {number2} e {number3}: {media:.2f}") #formata para duas casas decimais depois da virgula
-
 
---
+++ Aula02/script.py
@@ -0,0 +1,17 @@
+
+#--------------- AULA 02 --------------
+
+number1 = float(input("Digite um numero: "))
+
+number2 = float(input("Digite um numero: "))
+
+number3 = float(input("Digite um numero: "))
+
+media = (number1 + number2 + number3) / 3
+
+
+print(f"A média dos numeros {number1}, {number2} e {number3}: {media}") #usa a formatação padrão
+
+print(f"A média dos numeros {number1}, {number2} e {number3}: {media:.2f}") #formata para duas casas decimais depois da virgula
+
+
