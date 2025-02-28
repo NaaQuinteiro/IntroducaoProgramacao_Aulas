67763edf167f86017a6503d1ed9e637e910efc33
NaaQuinteiro <thaynaquinteiro@gmail.com>
2025-02-27 21:33:14 -0300

    Aula 04

---
+++ Aula04/exercicio_01.py
@@ -0,0 +1,9 @@
+#Tabuada 
+
+tabuada = int(input("Insira o número que deseja calcular a tabuada: "))
+
+i = 0
+
+while i < 10:
+    print(f"{tabuada} x {i} = {tabuada * i}"  )
+    i += 1
--- Aula04/lacos.py
+++ Aula04/lacos.py
@@ -3,3 +3,14 @@ i = 1
 while i <= 10:
     print(i)
     i += 1
+
+
+while True:
+    print("Estou em um loop")
+    
+    sair = input("Quer sair?")    
+    if sair == "Sim":
+        print("Bye")
+        break
+    else:
+        continue
